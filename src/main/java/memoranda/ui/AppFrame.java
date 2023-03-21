fd0cad1bc698c558bb89ddd159d4781eaef2e2e7
brandonactipis <bactipis@asu.edu>
2023-03-20 19:10:25 -0700

    Changed the "Help" toolbar item to "Support"

--- src/main/java/memoranda/ui/AppFrame.java
+++ src/main/java/memoranda/ui/AppFrame.java
@@ -274,7 +274,7 @@ public class AppFrame extends JFrame {
                 doExit();
             }
         });
-        jMenuHelp.setText(Local.getString("Help"));
+        jMenuHelp.setText(Local.getString("Support"));
         
         jMenuHelpGuide.setText(Local.getString("Online user's guide"));
         jMenuHelpGuide.setIcon(new ImageIcon(AppFrame.class.getResource(
